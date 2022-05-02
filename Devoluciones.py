97b63d6772d44a9bdba27ac305274b953684982a
Ricardo Salinas  Marin <100739835+Richysm99@users.noreply.github.com>
2022-05-02 11:35:32 -0500

    Clase Devoluciones

---
+++ Devoluciones.py
@@ -0,0 +1,27 @@
+# Clase Devoluciones
+ 
+class Devoluciones:
+    IdDevoluciones: bool
+    Fecha: DateTime
+
+    def __init__(self):
+        self.Factura = Factura ()
+
+        def Ingresar(self):
+            return "Ingresar"
+
+        def Modificar(self):
+            return "Modificar"
+
+        def Eliminar(self):
+            return "Eliminar"
+        
+        def Consultar(self):
+            return "Consultar"
+
+        def Producto(self.Producto):
+            return "Producto"
+        
+        def CarritoCompras(self.Producto):
+            return "Carrito Compras"
+            
