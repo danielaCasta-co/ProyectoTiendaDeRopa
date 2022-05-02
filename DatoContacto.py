30737ab79605ceb731e4c50c06843c87949b8263
Ricardo Salinas  Marin <100739835+Richysm99@users.noreply.github.com>
2022-05-02 11:34:41 -0500

    Clase DatoContacto

---
+++ DatoContacto.py
@@ -0,0 +1,16 @@
+# Clase DatoContacto
+
+class DatoContacto:
+    Telefono: int
+    Direccion: str
+    Email: str
+    UsuarioPagina: str
+    ContraseñaPagina: str
+
+    def ActualizarDatoContacto(self):
+            return "Actualizar Dato Contacto"
+
+    def ValidarEmail(self):
+            return "Validar Email"
+            
+            
