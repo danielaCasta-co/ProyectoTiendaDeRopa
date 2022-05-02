fba7b6bc895ec38c3922b4c4640650bbb95a26e9
Ricardo Salinas  Marin <100739835+Richysm99@users.noreply.github.com>
2022-05-02 11:36:48 -0500

    Clase Producto

---
+++ Producto.py
@@ -0,0 +1,14 @@
+# Clase Producto
+    
+class Producto:
+    ReferenciaProducto: int
+    NombreProducto: str
+    Categoria: str
+    Genero: str
+    Talla: str
+    Color: str
+    Marca: str
+
+    def ModificarProducto(self):
+            return "Modificar Producto"
+            
