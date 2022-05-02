c9dba47a35d9e6dcfccbca2abbaed5b40de4f0c9
danielaCasta-co <101675853+danielaCasta-co@users.noreply.github.com>
2022-05-02 12:10:15 -0500

    Clase Administrador

---
+++ Administrador.py
@@ -0,0 +1,9 @@
+class Administrador:
+    Usuario: str
+    Contraseña: bool
+
+    def CambiarContraseña(self):
+            return "Cambiar Contraseña"
+
+    def EliminarAdministrador(self):
+            return "Eliminar Administrador"
