f6314b4a7f2871eb9603ee31c3af07be2fff43d3
Laura Daniela Castaño Garcia <101675853+danielaCasta-co@users.noreply.github.com>
2022-05-02 12:14:46 -0500

    Clase DetalleFactura

---
+++ DetalleFactura.py
@@ -0,0 +1,10 @@
+class DetalleFactura:
+    ValorUnitario: int
+    CantidadProducto: int
+
+    def __init__(self):
+        self.Producto = Producto()
+        self.Factura = Factura()
+
+        def Producto(self.Producto):
+            return "Producto"
