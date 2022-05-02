be6015f073a1ddb7b7e9f4c04caa7a5ea8922f04
Laura Daniela Castaño Garcia <101675853+danielaCasta-co@users.noreply.github.com>
2022-05-02 12:12:51 -0500

    Clase Cliente

---
+++ Cliente.py
@@ -0,0 +1,12 @@
+class Cliente:
+
+    Nombre: str
+    NumeroTarjeta: int
+    Edad: int
+    Sexo: str
+
+    def EditarCliente(self):
+            return "Editar Cliente"
+
+    def EliminarCliente(self):
+            return "Eliminar Cliente"
