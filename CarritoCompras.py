ebdd3037e32eb146363124fa12328265a824d541
Ricardo Salinas  Marin <100739835+Richysm99@users.noreply.github.com>
2022-05-02 11:33:33 -0500

    Clase CarritoCompras

---
+++ CarritoCompras.py
@@ -0,0 +1,19 @@
+# Clase CarritoCompras
+
+class CarritoCompras:
+    Codigo: int
+
+    def __init__(self):
+        self.Usuario = Usuario()
+        self.Pedido = Pedido()
+        self.Factura = Factura()
+        self.MetodoPago = MetdoPago()
+
+        def ReiniciarCarritoCompras(self):
+            return "Reiniciar Carrito Compras"
+        
+        def Usuario(self.Usuario):
+            return "Usuario"
+        
+        def Producto(self.Producto):
+            return "Producto"
