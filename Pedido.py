6162e4facdb7fd1a73741d9b9625247e403baca9
juancavalencia26 <99616775+juancavalencia26@users.noreply.github.com>
2022-05-03 12:38:06 -0500

    Clase Pedido

---
+++ Pedido.py
@@ -0,0 +1,12 @@
+class Pedido:
+    Cantidad: int
+
+    def __init__(self):
+        self.Producto = Producto ()
+        self.PedidoCliente = Usuario ()
+
+        def ModificarPedido(self):
+            return "Modificar Pedido"
+
+        def ReiniciarFactura(self):
+            return "Reiniciar Factura"
