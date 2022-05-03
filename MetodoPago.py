aacff61a4965e3d3beba93d5ed7587d9ecf7533d
juancavalencia26 <99616775+juancavalencia26@users.noreply.github.com>
2022-05-03 12:35:07 -0500

    Clase MetodoPago

---
+++ MetodoPago.py
@@ -0,0 +1,11 @@
+ MetodoPago
+
+class MetodoPago:
+    IdPago: int
+    TipoPago: str
+
+    def EliminarMetodoPago(self):
+            return "Eliminar Metodo Pago"
+      
+    def CrearMetodoPago(self):
+      return "Crear Metodo Pago"
