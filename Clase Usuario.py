b018ae68dab4a7c5b80f043c1a97d945f7bfc936
juancavalencia26 <99616775+juancavalencia26@users.noreply.github.com>
2022-05-03 12:39:12 -0500

    Clase Usuario

---
+++ Clase Usuario.py
@@ -0,0 +1,9 @@
+class Usuario:
+    IdUsuario: int
+    FechaRegistro: int
+
+        def EliminarUsuario(self):
+            return "Eliminar Usuario"
+
+        def ValidarAdministrador(self):
+            return "Validar Administrador"
