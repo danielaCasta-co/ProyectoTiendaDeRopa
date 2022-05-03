589539351454e593b0777304b752d0e82b328fd6
juancavalencia26 <99616775+juancavalencia26@users.noreply.github.com>
2022-05-03 13:12:07 -0500

    Corrección Clase Usuario

--- Usuario.py
+++ Usuario.py
@@ -1,9 +1,27 @@
+from ast import Pass
+
+
 class Usuario:
     IdUsuario: int
     FechaRegistro: int
+
+    def __init__(self):
+        self.DatoContacto = DatoContacto
+
+        def CrearUsuario(self):
+            return "Crear Usuario"
 
         def EliminarUsuario(self):
             return "Eliminar Usuario"
 
         def ValidarAdministrador(self):
             return "Validar Administrador"
+        
+class Usuario:
+    pass
+
+class Cliente(Usuario):
+    pass
+
+class Administrador(Usuario):
+    pass
