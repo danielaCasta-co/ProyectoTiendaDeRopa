e7977d9925b201df019ae549e5873295fdf756b5
danielaCasta-co <ldcastano96290@umanizales.edu.co>
2022-05-24 22:45:43 -0500

    commit question

---
+++ Parcial3/Persona.py
@@ -0,0 +1,20 @@
+class Persona():
+    def __init__(self,Nombre, Nif, FechaNac):
+        self.Nombre = Nombre
+        self.Nif = Nif
+        self.FechaNac = FechaNac
+        
+class Jugador():
+    def __init__(self, NumFed):
+        self.NumFed = NumFed
+        
+class Nombre():
+    # def __init__():     
+          
+        
+class Nif():
+    # def __init__():     
+        
+class FechaNac():
+    # def __init__():                
+        
